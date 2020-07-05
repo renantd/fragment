19633f3afa7af44ea9e15c5308d8b9acf9aa6345
Renan Costa e Silva <63022300+renantd@users.noreply.github.com>
2020-07-05 12:38:09 -0300

    Exemplo de fragmaent no android

---
+++ app/src/main/java/br/sofex/com/fragment/MainActivity2.java
@@ -0,0 +1,21 @@
+package br.sofex.com.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.sofex.com.fragment.ui.mainactivity2.MainActivity2Fragment;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity2_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainActivity2Fragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/br/sofex/com/fragment/ui/mainactivity2/MainActivity2ViewModel.java
@@ -0,0 +1,7 @@
+package br.sofex.com.fragment.ui.mainactivity2;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivity2ViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
